29d4ac4632b0f38bbaff14b3783ad0b929cdcca8
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2019-02-03 17:32:31 +0200

    Front end build

---
+++ build/precache-manifest.1506672fe4116affa4e11692b6f31905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c268d3e3bc3982fbac0",
+    "url": "/static/css/main.72a6e436.chunk.css"
+  },
+  {
+    "revision": "3c268d3e3bc3982fbac0",
+    "url": "/static/js/main.3c268d3e.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91a537f27ddf90abaf3eb9c47087adc3",
+    "url": "/index.html"
+  }
+];
