10ae685ddd3e000aefc2937e6a53513f345b655e
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2019-02-03 16:57:37 +0200

    No port for Now

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// const PORT = 3001
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
+app.listen()
